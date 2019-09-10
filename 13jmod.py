9f7adea5421391aea7b56e89b0376a0209908e96
venkatnaveen556 <41520793+venkatnaveen556@users.noreply.github.com>
2019-09-10 10:10:24 +0530

    Add files via upload

---
+++ 13jmod.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def add(i,j):
+    return i+j
+
+
+# In[7]:
+
+
+def sub(i,j):
+    return i-j
+
+
+# In[8]:
+
+
+def mul(i,j):
+    return i*j
+
+
+# In[4]:
+
+
+def div(i,j):
+    if(j!=0):
+        return i/j
+    else:
+        return "not divisible by zero"
+
+
+# In[ ]:
+
+
+
+
